2c7b495a4403368b5eb655192ab80d70cdf55d38
eunbin-hyun <eunbin8592@gmail.com>
2025-06-10 17:53:05 +0900

    Update 1_install_halio.sh

--- 파일변환/yolo2hef/1_install_halio.sh
+++ 파일변환/yolo2hef/1_install_halio.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-pip install ./hailo_dataflow_compiler-3.29.0-py3-none-linux_x86_64.whl
+pip install ./hailo_dataflow_compiler-3.27.0-py3-none-linux_x86_64.whl
 pip install ./hailort-4.19.0-cp310-cp310-linux_x86_64.whl
